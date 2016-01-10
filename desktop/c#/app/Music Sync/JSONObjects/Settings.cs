adda59d0c6127ab3a007bf825b88ad4f51239df4
Wassup789 <therealwassup789@gmail.com>
2016-01-10 14:48:02 -0800

    v1.0.0.0 Release
    Added a Windows application and installer to create a better alternative
    to the PHP server.
    Changed the logo with distracting shadow

---
+++ desktop/c#/app/Music Sync/JSONObjects/Settings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicSync.JSONObjects
+{
+    class Settings
+    {
+        public int port { get; set; }
+        public SettingsPlaylist[] playlists { get; set; }
+    }
+}
---
+++ desktop/c#/app/Music Sync/JSONObjects/WebPlaylist.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicSync.JSONObjects
+{
+    class WebPlaylist
+    {
+        public string name { get; set; }
+        public string name_b64 { get; set; }
+        public int files { get; set; }
+
+        public WebPlaylist(string playlistName, string playlistName_b64, int fileCount)
+        {
+            name = playlistName;
+            name_b64 = playlistName_b64;
+            files = fileCount;
+        }
+    }
+}
